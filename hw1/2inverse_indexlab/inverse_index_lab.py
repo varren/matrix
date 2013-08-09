3bb68df8761b60d4acd543a45cfe8b3688faa8e6
Varren <varren@gmail.com>
2013-08-09 10:35:45 +0400

    Done Mat.py hw

---
+++ hw1/2inverse_indexlab/inverse_index_lab.py
@@ -0,0 +1,56 @@
+from random import randint
+from dictutil import *
+
+## Task 1
+def movie_review(name):
+    """
+    Input: the name of a movie
+    Output: a string (one of the review options), selected at random using randint
+    """
+    review_options = ["See it!", "A gem!", "Ideological claptrap!"]
+    return review_options[randint(0, len(review_options) - 1)]
+
+## Tasks 2 and 3 are in dictutil.py
+
+## Task 4    
+def makeInverseIndex(strlist):
+    """
+    Input: a list of documents as strings
+    Output: a dictionary that maps each word in any document to the set consisting of the
+            document ids (ie, the index in the strlist) for all documents containing the word.
+
+    Note that to test your function, you are welcome to use the files stories_small.txt
+      or stories_big.txt included in the download.
+    """
+    index = dict()    
+    for (idx, doc) in enumerate(strlist):
+        words = doc.split();
+        for word in words:
+            s = index.setdefault(word, set())
+            s.add(idx)
+    return index
+
+## Task 5
+def orSearch(inverseIndex, query):
+    """
+    Input: an inverse index, as created by makeInverseIndex, and a list of words to query
+    Output: the set of document ids that contain _any_ of the specified words
+    """
+    s = set()
+    for y in [inverseIndex[q] for q in query]:
+        s.update(y)
+    return s
+
+## Task 6
+def andSearch(inverseIndex, query):
+    """
+    Input: an inverse index, as created by makeInverseIndex, and a list of words to query
+    Output: the set of all document ids that contain _all_ of the specified words
+    """
+    s = set()
+    for y in [inverseIndex[q] for q in query]:
+        if len(s) == 0:
+            s.update(y)
+        else:
+            s.intersection_update(y)
+    return s
---
+++ hw3/hw2.py
@@ -0,0 +1,99 @@
+# version code 761
+# Please fill out this stencil and submit using the provided submission script.
+
+from vec import Vec
+import itertools
+from GF2 import one
+
+
+## Problem 1
+def vec_select(veclist, k): 
+    '''
+    >>> D = {'a','b','c'}
+    >>> v1 = Vec(D, {'a': 1})
+    >>> v2 = Vec(D, {'a': 0, 'b': 1})
+    >>> v3 = Vec(D, {        'b': 2})
+    >>> v4 = Vec(D, {'a': 10, 'b': 10})
+    >>> vec_select([v1, v2, v3, v4], 'a') == [Vec(D,{'b': 1}), Vec(D,{'b': 2})]
+    True
+    '''
+    return [x for x in veclist if x[k] == 0]
+
+def vec_sum(veclist, D): 
+    '''
+    >>> D = {'a','b','c'}
+    >>> v1 = Vec(D, {'a': 1})
+    >>> v2 = Vec(D, {'a': 0, 'b': 1})
+    >>> v3 = Vec(D, {        'b': 2})
+    >>> v4 = Vec(D, {'a': 10, 'b': 10})
+    >>> vec_sum([v1, v2, v3, v4], D) == Vec(D, {'b': 13, 'a': 11})
+    True
+    '''
+    return sum(veclist) if veclist else Vec(D,{})
+
+def vec_select_sum(veclist, k, D): 
+    '''
+    >>> D = {'a','b','c'}
+    >>> v1 = Vec(D, {'a': 1})
+    >>> v2 = Vec(D, {'a': 0, 'b': 1})
+    >>> v3 = Vec(D, {        'b': 2})
+    >>> v4 = Vec(D, {'a': 10, 'b': 10})
+    >>> vec_select_sum([v1, v2, v3, v4], 'a', D) == Vec(D, {'b': 3})
+    True
+    '''
+    return vec_sum(vec_select(veclist,k), D) 
+
+## Problem 2
+def scale_vecs(vecdict):
+    '''
+    >>> v1 = Vec({1,2,3}, {2: 9})
+    >>> v2 = Vec({1,2,4}, {1: 1, 2: 2, 4: 8})
+    >>> scale_vecs({3: v1, 5: v2}) == [Vec({1,2,3},{2: 3.0}), Vec({1,2,4},{1: 0.2, 2: 0.4, 4: 1.6})]
+    True
+    '''
+    return [(1.0/x) * vecdict[x]  for x in vecdict]
+
+
+## Problem 3
+def GF2_span(D, L):
+    '''
+    >>> from GF2 import one
+    >>> D = {'a', 'b', 'c'}
+    >>> L = [Vec(D, {'a': one, 'c': one}), Vec(D, {'b': one})]
+    >>> len(GF2_span(D, L))
+    4
+    >>> Vec(D, {}) in GF2_span(D, L)
+    True
+    >>> Vec(D, {'b': one}) in GF2_span(D, L)
+    True
+    >>> Vec(D, {'a':one, 'c':one}) in GF2_span(D, L)
+    True
+    >>> Vec(D, {x:one for x in D}) in GF2_span(D, L)
+    True
+    '''
+    gf2pare = [x for x in itertools.product([one,0], repeat = len(L))]
+    return [ vec_sum([a[x]*L[x] for x in range(len(L))], D) for a in gf2pare]
+D = {'a', 'b', 'c'} 
+L = [Vec(D, {'a': one, }), Vec(D, {'a': one, 'c': one}), Vec(D, {'b': one})]  
+ 
+print("Input:"+ str(D) +" "+ str(L) )
+
+print("Output:" + str(GF2_span(D, L)) )
+
+## Problem 4
+# Answer with a boolean, please.
+
+is_it_a_vector_space_1 = True
+is_it_a_vector_space_2 = False
+
+
+
+## Problem 5
+is_it_a_vector_space_3 = True
+is_it_a_vector_space_4 = False
+
+
+## Problem 6
+
+is_it_a_vector_space_5 = True
+is_it_a_vector_space_6 = False
